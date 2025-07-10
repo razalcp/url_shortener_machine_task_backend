aa84d88ca41aef468052f3ddd67e782fe09c0059
razalcp <cprazalnazim@gmail.com>
2025-07-10 12:38:06 +0530

    jwt type fixed

--- src/config/jwt_config.ts
+++ src/config/jwt_config.ts
@@ -17,21 +17,30 @@
 
 // export { createToken, createRefreshToken }
 
-import jwt from 'jsonwebtoken'
+import jwt, { Secret, SignOptions } from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import type { StringValue } from 'ms'  
 
 dotenv.config()
 
-const secret_key = process.env.JWT_SECRET || 'default_secret'
-const accessTokenTime = process.env.ACCESS_TOKEN_EXPIREY_TIME || '1h'
-const refreshTokenTime = process.env.REFRESH_TOKEN_EXPIREY_TIME || '7d'
+const secret_key: Secret = process.env.JWT_SECRET || 'default_secret'
+
+
+const accessTokenTime = (process.env.ACCESS_TOKEN_EXPIREY_TIME || '15m') as StringValue
+const refreshTokenTime = (process.env.REFRESH_TOKEN_EXPIREY_TIME || '7d') as StringValue
 
 const createToken = (user_id: string, role: string): string => {
-  return jwt.sign({ user_id, role }, secret_key, { expiresIn: accessTokenTime })
+  const payload = { user_id, role }
+  const options: SignOptions = { expiresIn: accessTokenTime }
+
+  return jwt.sign(payload, secret_key, options)
 }
 
 const createRefreshToken = (user_id: string, role: string): string => {
-  return jwt.sign({ user_id, role }, secret_key, { expiresIn: refreshTokenTime })
+  const payload = { user_id, role }
+  const options: SignOptions = { expiresIn: refreshTokenTime }
+
+  return jwt.sign(payload, secret_key, options)
 }
 
 export { createToken, createRefreshToken }
